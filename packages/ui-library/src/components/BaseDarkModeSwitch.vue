835248f5e36b0f24a3fb0ee8bc58b7749459d4ff
timothy okooboh <okoobohtimothy@gmail.com>
2023-12-17 14:38:50 +0100

    feat:create base dark mode switch componenr

---
+++ packages/ui-library/src/components/BaseDarkModeSwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center">
+    <img src="/sun.svg" alt="sun" />
+    <button
+      class="group bg-white flex items-center mx-4 px-[5px] w-[48px] h-[24px] rounded-[25px]"
+      @click="$emit('update:isDark', !isDark)"
+    >
+      <div
+        class="h-[14px] w-[14px] rounded-full bg-primary-violet-200 duration-200 group-hover:bg-primary-violet-100"
+        :class="{ 'translate-x-[25px]': isDark }"
+      />
+    </button>
+    <img src="/moon.svg" alt="moon" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  isDark: Boolean,
+});
+defineEmits(["update:isDark"]);
+</script>
