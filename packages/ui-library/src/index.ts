5f8a34242bfe4334fa86d20c500e3284f963cc79
timothy okooboh <okoobohtimothy@gmail.com>
2023-12-17 17:40:55 +0100

    build library

---
+++ packages/ui-library/src/index.ts
@@ -0,0 +1,13 @@
+import BaseButton from "./components/BaseButton.vue";
+import BaseInput from "./components/BaseInput.vue";
+import BaseInputGroup from "./components/BaseInputGroup.vue";
+import BaseCheckbox from "./components/BaseCheckbox.vue";
+import BaseDarkModeSwitch from "./components/BaseDarkModeSwitch.vue";
+
+export {
+  BaseButton,
+  BaseInput,
+  BaseInputGroup,
+  BaseCheckbox,
+  BaseDarkModeSwitch,
+};
--- packages/ui-library/vite.config.ts
+++ packages/ui-library/vite.config.ts
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  plugins: [vue(), dts()],
+  build: {
+    outDir: "./dist",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ui-library",
+      // We are building for CJS and ESM, use a function to rename automatically files.
+      // Example: my-component-library.esm.js
+      fileName: (format) => `${"ui-library"}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue", "tailwindcss"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
