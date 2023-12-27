824e5c345eb7c5f329906fa4b78ceb57aff9bcc6
timothy okooboh <okoobohtimothy@gmail.com>
2023-12-27 15:39:24 +0100

    improve responsiveness

--- apps/jobs/components/JobList.vue
+++ apps/jobs/components/JobList.vue
@@ -8,12 +8,14 @@ defineProps<{
 
 <template>
   <div>
-    <div class="grid gap-y-[49px] gap-x-[11px] md:grid-cols-2 lg:grid-cols-3">
+    <div
+      class="md:grid gap-y-[49px] gap-x-[11px] md:grid-cols-2 lg:grid-cols-3"
+    >
       <JobListItem
         v-for="job in jobs"
         :key="job.id"
         v-bind="job"
-        class="hover:translate-y-[-5px] duration-200"
+        class="hover:translate-y-[-5px] duration-200 w-full"
       />
     </div>
   </div>
